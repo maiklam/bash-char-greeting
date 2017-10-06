adf7faf0e8ee4a427a543f2b67a2eefb4dd52af4
Mai Lam <mlam@thoughtworks.com>
2017-10-07 05:46:18 +1100

    fix check script

--- checks.sh
+++ checks.sh
@@ -11,8 +11,8 @@ mkdir -p /tmp/.greeting
 cat ${DIR}/greetings.txt | cut -d , -f2 | firstLetters  > '/tmp/.greeting/first-letters-nationalities'
 ${LS} ${DIR}/characters | firstLetters > '/tmp/.greeting/first-letters-characters'
 
-lettersMissingFromCharacters=`comm -i -2 -3 /tmp/.greeting/first-letters-nationalities /tmp/.greeting/first-letters-characters`
-lettersMissingFromNationalities=`comm -i -1 -3 /tmp/.greeting/first-letters-nationalities /tmp/.greeting/first-letters-characters`
+lettersMissingFromCharacters=`comm -1 -2 -3 /tmp/.greeting/first-letters-nationalities /tmp/.greeting/first-letters-characters`
+lettersMissingFromNationalities=`comm -1 -1 -3 /tmp/.greeting/first-letters-nationalities /tmp/.greeting/first-letters-characters`
 
 if [ -n "${lettersMissingFromCharacters}" ]; then
   echo "WARN: Missing characters starting with the following letters: ${lettersMissingFromCharacters}"
